3e5481a2ff4364b137a860fe82d9a7a35ce3dcbf
Eryl <eryl.murphy@gmail.com>
2018-05-20 18:45:42 -0400

    did homework

--- main.js
+++ main.js
@@ -4,30 +4,79 @@ var deck = [];
 var players = [];
 var hands = [];
 
-function buildDeck() {
 
-};
+// `buildDeck`: Populates the`deck` array with 52 standard playing cards.
+//     _NOTE_: each 'card' in the`deck` array should be an object with a value and
+// suite property.
+
+function buildDeck() {
+values.forEach = (value) => {
+suits.forEach = (suit) => {
+    let card = [{suit: suit, value: value,}];
+    deck.push(card);
+}}};
+
+
+
+// shuffleDeck: Randomizes the order of deck.
+
 
 function shuffleDeck() {
+    let i = 0;
+    let j = 0;
+    let shuffledDeck = null;
 
-};
+    for (i = deck.length - 1; i > 0; i --) {
+        j = Math.floor(Math.random() * (i + 1))
+        shuffledDeck = deck[i];
+        deck[i] = deck[j];
+        deck[j] = shuffledDeck;
+}};
+
 
 function getMorePlayers() {
-
+let playerName;
+    do {
+        if (playerName) {
+            players.push({ name: playerName });
+        }
+        playerName = prompt('Add a player and hit <enter>. Type "STOP" when finished');
+    } while (playerName && playerName !== 'STOP');
 };
 
+
+// deal`: Assigns one card to each player.
 function deal() {
-
+    players.forEach = () => {
+        players.card = deck.pop();
+    }
 };
+
+
 
 function findHighestCard() {
-
+    players.sort = () => {
+        return players[0].card < players[1].card;
+    };
 };
+
 
 function announceWinners() {
-
+let message = '';
+    players.forEach(() => {
+        let message = `${players.playerName} came in ${i + 1} place with the ${players.card.value} of ${players.card.suit}!`
+    });
+    alert(message);
 };
+
 
 function playANewGame() {
-
+    buildDeck();
+    shuffleDeck();
+    getMorePlayers();
+    deal();
+    findHighestCard();
+    announceWinners();
 };
+
+playANewGame();
